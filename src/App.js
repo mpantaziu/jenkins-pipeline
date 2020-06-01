ae132a3b9ef98d01ec67e7be0d0a92d8533a0d72
Fredy-SSA <alin@zizo.ro>
2020-06-01 19:03:49 +0300

    Sample NODE Js CI Test 06/01/2020 19:03:49

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3>Sample Code</h3>
       </div>
     );
   }
