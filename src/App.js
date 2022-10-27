d29581c7b72201771bb4f3071bab54a862a5b460
Fredy-SSA <56113098+Fredy-SSA@users.noreply.github.com>
2022-10-27 13:42:26 +0300

    Update App.js
    Restore

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React !  Update  versiune Noua </h1>
+          <h1 className="App-title">Welcome to React !  </h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <h3>Sample Code</h3>
-        <h3>Have the best day ever and if someone tries to ruin it for you, just tell me. I will yell at them from a safe distance, ’cause that’s what friends do, yo!</h3>
+        
       </div>
     );
   }
